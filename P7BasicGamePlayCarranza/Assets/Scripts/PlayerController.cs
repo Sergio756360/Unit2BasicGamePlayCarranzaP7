8a9c88877cbb8ac56290ab6e3c32a6881959a61b
Sergio756360 <756360@student.dusd.net>
2026-03-06 15:25:30 -0800

    New Changes
    added some new changes for the project including new scripts

--- P7BasicGamePlayCarranza/Assets/Scripts/PlayerController.cs
+++ P7BasicGamePlayCarranza/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float leftBoundary = -10.0f;
-    public float rightBoundary = -10.0f;
     public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,16 +18,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < leftBoundary)
-        {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
-        }
-
-        if (transform.position.x > rightBoundary)
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
     }
 }
