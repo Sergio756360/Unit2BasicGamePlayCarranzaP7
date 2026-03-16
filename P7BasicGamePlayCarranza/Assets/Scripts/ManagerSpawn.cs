02df743d6491162deb9467a66b4704841f849d76
Sergio756360 <756360@student.dusd.net>
2026-03-16 15:27:19 -0700

    Spawn Manager
    New changes including the spawn manager which spawns in animals

--- P7BasicGamePlayCarranza/Assets/Scripts/ManagerSpawn.cs
+++ P7BasicGamePlayCarranza/Assets/Scripts/ManagerSpawn.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class ManagerSpawn : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +16,12 @@ public class ManagerSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos , animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
